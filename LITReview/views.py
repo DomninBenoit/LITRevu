55158f993d119990cf0e7f3190e4053420c2cf08
Domnin <dombouif@gmail.com>
2023-08-28 12:32:15 +0200

    Add models and register/login page

---
+++ LITReview/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+
+class RegistrationView(CreateView):
+    form_class = UserCreationForm  # Utilisez le formulaire d'inscription par défaut de Django
+    template_name = 'registration/register.html'  # Chemin vers le template d'enregistrement
+    success_url = reverse_lazy('login')  # Rediriger vers la page de connexion après l'enregistrement
